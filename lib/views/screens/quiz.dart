ad1bb11a12d69c7997b4cc89c390dd4226e94f89
alokyadav686 <alokkumaryadav2799@gmail.com>
2025-05-04 01:15:16 +0530

    quiz comming soon

--- lib/views/screens/quiz.dart
+++ lib/views/screens/quiz.dart
@@ -2,7 +2,6 @@ import 'package:ccc_app/components/background%20Animation/bganimation.dart';
 import 'package:ccc_app/constants/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class Quiz extends StatefulWidget {
   const Quiz({super.key});
@@ -15,9 +14,20 @@ class _QuizState extends State<Quiz> {
   
   @override
   Widget build(BuildContext context) {
-    double widthh = MediaQuery.of(context).size.width ;
-    return Center(
-      
+    return Scaffold(
+      backgroundColor: AppColors.primaryColor,
+      body: Stack(
+        children: [
+          CircleRotationAnimation(),
+          SingleChildScrollView(
+            child: Column(
+              children: [
+                SvgPicture.asset("assets/images/quizz.svg")
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
