76aa468377b4c3f345d1dd730ca268846b31a5b3
samit18 <samitsrivastava59@gmail.com>
2025-05-04 23:46:29 +0530

    initial commit

--- lib/views/explore_button/domain.dart
+++ lib/views/explore_button/domain.dart
@@ -34,38 +34,43 @@ class DomainDetails extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final List<Map<String, String>> domains = [
+    final List<Map<String, dynamic>> domains = [
       {
         "title": "App Development",
         "description":
-            "App development involves designing, building, and maintaining software applications that run on various devices, including smartphones, tabs, etc."
+            "App development involves designing, building, and maintaining software applications that run on various devices, including smartphones, tabs, etc.",
+        "color": Color(0xFF00BCD4),
       },
       {
         "title": "Web Development",
         "description":
-            "Web development is the process of creating and maintaining websites or web applications that are accessible through the internet."
+            "Web development is the process of creating and maintaining websites or web applications that are accessible through the internet.",
+        "color": Color(0xFFFF5722),
       },
       {
         "title": "Cloud Computing",
         "description":
-            "Cloud computing is the delivery of computing services—such as storage, processing, databases, networking, and software over the internet."
+            "Cloud computing is the delivery of computing services—such as storage, processing, databases, networking, and software over the internet.",
+        "color": Color(0xFF7C4DFF),
       },
       {
         "title": "Machine Learning",
         "description":
-            "Machine Learning (ML) is a branch of artificial intelligence (AI) that focuses on developing systems capable of learning and improving from experience."
+            "Machine Learning (ML) is a branch of artificial intelligence (AI) that focuses on developing systems capable of learning and improving from experience.",
+        "color": Color(0xFFCDDC39),
       },
       {
         "title": "UI/UX Design",
         "description":
-            "UI (User Interface) and UX (User Experience) design work together to ensure that a digital product is both visually appealing and easy to use."
+            "UI (User Interface) and UX (User Experience) design work together to ensure that a digital product is both visually appealing and easy to use.",
+        "color": Color(0xFFF50057),
       },
     ];
 
     final domain = domains[selectedIndex % domains.length];
 
     return Container(
-      color: Colors.white,
+      color: Colors.black,
       padding: const EdgeInsets.all(16),
       child: Stack(
         children: [
@@ -83,37 +88,45 @@ class DomainDetails extends StatelessWidget {
                   width: 24,
                   height: 24,
                   decoration: const BoxDecoration(
-                    color: Color.fromRGBO(51, 34, 104, 1),
+                    color: Colors.white,
                     shape: BoxShape.circle,
                   ),
                   child:
-                      const Icon(Icons.circle, size: 12, color: Colors.white),
+                      const Icon(Icons.circle, size: 12, color: Colors.black),
                 ),
                 const SizedBox(width: 16),
                 Expanded(
                   child: SingleChildScrollView(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          domain["title"]!,
-                          style: const TextStyle(
-                            fontWeight: FontWeight.w700,
-                            fontSize: 24,
-                            color: Colors.black,
+                    child: AnimatedSwitcher(
+                      duration: const Duration(milliseconds: 500),
+                      transitionBuilder:
+                          (Widget child, Animation<double> animation) {
+                        return FadeTransition(opacity: animation, child: child);
+                      },
+                      child: Column(
+                        key: ValueKey<int>(selectedIndex),
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            domain["title"],
+                            style: TextStyle(
+                              fontWeight: FontWeight.w700,
+                              fontSize: 24,
+                              color: domain["color"],
+                            ),
                           ),
-                        ),
-                        const SizedBox(height: 8),
-                        Text(
-                          domain["description"]!,
-                          style: const TextStyle(
-                            fontSize: 14,
-                            fontWeight: FontWeight.w500,
-                            color: Colors.black,
+                          const SizedBox(height: 8),
+                          Text(
+                            domain["description"],
+                            style: const TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.w500,
+                              color: Colors.white, // Updated color
+                            ),
                           ),
-                        ),
-                        const SizedBox(height: 80),
-                      ],
+                          const SizedBox(height: 80),
+                        ],
+                      ),
                     ),
                   ),
                 ),
@@ -152,25 +165,54 @@ class ThreadPainter extends CustomPainter {
   bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
 }
 
-class DomainPhoto extends StatelessWidget {
+class DomainPhoto extends StatefulWidget {
+  DomainPhoto({super.key});
+
+  @override
+  State<DomainPhoto> createState() => _DomainPhotoState();
+}
+
+class _DomainPhotoState extends State<DomainPhoto>
+    with SingleTickerProviderStateMixin {
   final List<String> images = [
-    "assets/images/app.png",
-    "assets/images/web.png",
-    "assets/images/cloud.png",
-    "assets/images/ML.png",
-    "assets/images/ui.png",
+    "assets/images/app.jpg",
+    "assets/images/web.jpg",
+    "assets/images/cloud.jpg", 
+    "assets/images/machine.jpg",
+    "assets/images/UI UX.jpg",
   ];
 
   final CardController controller = CardController();
+  late final AnimationController _animationController;
+  late final Animation<double> _floatingAnimation;
 
-  DomainPhoto({super.key});
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 3),
+    )..repeat(reverse: true);
+
+    _floatingAnimation =
+        Tween<double>(begin: 0.0, end: 10.0).animate(CurvedAnimation(
+      parent: _animationController,
+      curve: Curves.easeInOut,
+    ));
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     final DomainController domainController = Get.find();
 
     return Container(
-      color: AppColors.primaryColor,
+      color: Colors.black,
       height: MediaQuery.of(context).size.height / 2.5,
       child: TinderSwapCard(
         swipeUp: true,
@@ -183,14 +225,23 @@ class DomainPhoto extends StatelessWidget {
         maxWidth: MediaQuery.sizeOf(context).width * 0.9,
         minWidth: MediaQuery.sizeOf(context).width * 0.8,
         cardBuilder: (context, index) {
-          return Card(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(20),
-            ),
-            clipBehavior: Clip.antiAliasWithSaveLayer,
-            child: Image.asset(
-              images[index % images.length],
-              fit: BoxFit.cover,
+          return AnimatedBuilder(
+            animation: _floatingAnimation,
+            builder: (context, child) {
+              return Transform.translate(
+                offset: Offset(0, -_floatingAnimation.value),
+                child: child,
+              );
+            },
+            child: Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+              clipBehavior: Clip.antiAliasWithSaveLayer,
+              child: Image.asset(
+                images[index % images.length],
+                fit: BoxFit.cover,
+              ),
             ),
           );
         },
@@ -209,7 +260,7 @@ class DomainHead extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: AppColors.primaryColor,
+      backgroundColor: Colors.black,
       body: Column(
         children: const [
           SizedBox(height: 8),
