c855b1b27b016aa1b3fba1a60d32fbd6d58e9327
alokyadav686 <alokkumaryadav2799@gmail.com>
2025-05-04 23:47:12 +0530

    scroll behavior changed

--- lib/views/explore_button/registration_screen/register_screen.dart
+++ lib/views/explore_button/registration_screen/register_screen.dart
@@ -21,150 +21,154 @@ class RegistrationPage extends StatelessWidget {
             end: Alignment.bottomRight,
           ),
         ),
-        child: SingleChildScrollView(
-          padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 20),
-          child: Form(
-            key: controller.formKey,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(
-                  height: 300,
-                  width: double.infinity,
-                  child: Lottie.asset('assets/lottie/register_animation.json'),
-                ),
-                Container(
-                  width: double.infinity,
-                  height: 60,
-                  decoration: BoxDecoration(
-                    color: Colors.blue[800],
-                    borderRadius: BorderRadius.circular(10),
+        child: GestureDetector(
+          onTap: () => FocusScope.of(context).unfocus(),
+          child: SingleChildScrollView(
+            keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+            padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 20),
+            child: Form(
+              key: controller.formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    height: 300,
+                    width: double.infinity,
+                    child: Lottie.asset('assets/lottie/register_animation.json'),
                   ),
-                  child: const Center(
-                    child: Text(
-                      "Register Now",
-                      style: TextStyle(
-                        fontSize: 26,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                        decoration: TextDecoration.underline,
+                  Container(
+                    width: double.infinity,
+                    height: 60,
+                    decoration: BoxDecoration(
+                      color: Colors.blue[800],
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: const Center(
+                      child: Text(
+                        "Register Now",
+                        style: TextStyle(
+                          fontSize: 26,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          decoration: TextDecoration.underline,
+                        ),
                       ),
                     ),
                   ),
-                ),
-                const SizedBox(height: 8),
-                const Center(
-                  child: Text("Excited for NIMBUS 2.0  !!",
-                      style: TextStyle(
-                          fontSize: 16,
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold)),
-                ),
-                const SizedBox(height: 4),
-                const Center(
-                  child: Text("CODE THE FRAME TO\nWIN EXCITING REWARDS",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(color: Colors.white70, fontSize: 14)),
-                ),
-                buildTextField(
-                    "Name", "Enter your Full Name", controller.nameController),
-                buildTextField("Student ID", "Enter your Student Number",
-                    controller.idController),
-                const SizedBox(height: 10),
-                Row(
-                  children: [
-                    Expanded(
-                        child: buildDropdown("Branch",
-                            controller.selectedBranch, controller.branches)),
-                    const SizedBox(width: 10),
-                    Expanded(
-                        child: buildDropdown("Section",
-                            controller.selectedSection, controller.sections)),
-                  ],
-                ),
-                buildTextField("E-mail ID", "Enter your College E-mail ID",
-                    controller.emailController),
-                buildTextField("Mobile Number", "Enter your Mobile Number",
-                    controller.mobileController),
-                const SizedBox(height: 20),
-                Row(
-                  children: [
-                    Expanded(
-                        child: buildDropdown(
-                            "Residence",
-                            controller.selectedResidence,
-                            controller.residences)),
-                    const SizedBox(width: 10),
-                    Expanded(
-                        child: buildDropdown("Gender",
-                            controller.selectedGender, controller.genders)),
-                  ],
-                ),
-                const SizedBox(height: 30),
-                Obx(() => !controller.showPaymentSection.value
-                    ? GestureDetector(
-                        onTap: () {
-                          if (controller.formKey.currentState!.validate() &&
-                              controller.selectedBranch.value != null &&
-                              controller.selectedSection.value != null &&
-                              controller.selectedResidence.value != null &&
-                              controller.selectedGender.value != null) {
-                            controller.showPaymentSection.value = true;
-                          } else {
-                            Get.snackbar("Missing Info",
-                                "Please fill all fields and make selections.",
-                                backgroundColor: Colors.orange,
-                                colorText: Colors.white);
-                          }
-                        },
-                        child: actionButton(
-                            "Next", Color(0xFFE96F6F), Color(0xFF6C4AB6)),
-                      )
-                    : Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const Text("Proceed to Payment",
-                              style: TextStyle(
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.white)),
-                          const SizedBox(height: 10),
-                          const Text(
-                              "Scan the QR code below using any UPI app to pay ₹100. After the payment is completed, enter your Transaction ID below.",
-                              style: TextStyle(color: Colors.white70)),
-                          const SizedBox(height: 20),
-                          Center(
-                              child: Image.asset('assets/images/payment_qr.png',
-                                  width: 200, height: 200)),
-                          const SizedBox(height: 20),
-                          buildTextField(
-                              "Transaction ID",
-                              "Enter Transaction ID",
-                              controller.transactionIdController),
-                          const SizedBox(height: 30),
-                          Obx(() => controller.isLoading.value
-                              ? const Center(
-                                  child: CircularProgressIndicator(
-                                      color: Colors.white))
-                              : GestureDetector(
-                                  onTap: () {
-                                    if (controller.transactionIdController.text
-                                        .isNotEmpty) {
-                                      controller.submitRegistration();
-                                    } else {
-                                      Get.snackbar("Missing Info",
-                                          "Please enter the Transaction ID.",
-                                          backgroundColor: Colors.red,
-                                          colorText: Colors.white);
-                                    }
-                                  },
-                                  child: actionButton("Submit",
-                                      Color(0xFF6C4AB6), Color(0xFFE96F6F)),
-                                )),
-                        ],
-                      )),
-                const SizedBox(height: 80),
-              ],
+                  const SizedBox(height: 8),
+                  const Center(
+                    child: Text("Excited for NIMBUS 2.0  !!",
+                        style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold)),
+                  ),
+                  const SizedBox(height: 4),
+                  const Center(
+                    child: Text("CODE THE FRAME TO\nWIN EXCITING REWARDS",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(color: Colors.white70, fontSize: 14)),
+                  ),
+                  buildTextField(
+                      "Name", "Enter your Full Name", controller.nameController),
+                  buildTextField("Student ID", "Enter your Student Number",
+                      controller.idController),
+                  const SizedBox(height: 10),
+                  Row(
+                    children: [
+                      Expanded(
+                          child: buildDropdown("Branch",
+                              controller.selectedBranch, controller.branches)),
+                      const SizedBox(width: 10),
+                      Expanded(
+                          child: buildDropdown("Section",
+                              controller.selectedSection, controller.sections)),
+                    ],
+                  ),
+                  buildTextField("E-mail ID", "Enter your College E-mail ID",
+                      controller.emailController),
+                  buildTextField("Mobile Number", "Enter your Mobile Number",
+                      controller.mobileController),
+                  const SizedBox(height: 20),
+                  Row(
+                    children: [
+                      Expanded(
+                          child: buildDropdown(
+                              "Residence",
+                              controller.selectedResidence,
+                              controller.residences)),
+                      const SizedBox(width: 10),
+                      Expanded(
+                          child: buildDropdown("Gender",
+                              controller.selectedGender, controller.genders)),
+                    ],
+                  ),
+                  const SizedBox(height: 30),
+                  Obx(() => !controller.showPaymentSection.value
+                      ? GestureDetector(
+                          onTap: () {
+                            if (controller.formKey.currentState!.validate() &&
+                                controller.selectedBranch.value != null &&
+                                controller.selectedSection.value != null &&
+                                controller.selectedResidence.value != null &&
+                                controller.selectedGender.value != null) {
+                              controller.showPaymentSection.value = true;
+                            } else {
+                              Get.snackbar("Missing Info",
+                                  "Please fill all fields and make selections.",
+                                  backgroundColor: Colors.orange,
+                                  colorText: Colors.white);
+                            }
+                          },
+                          child: actionButton(
+                              "Next", Color(0xFFE96F6F), Color(0xFF6C4AB6)),
+                        )
+                      : Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Text("Proceed to Payment",
+                                style: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white)),
+                            const SizedBox(height: 10),
+                            const Text(
+                                "Scan the QR code below using any UPI app to pay ₹100. After the payment is completed, enter your Transaction ID below.",
+                                style: TextStyle(color: Colors.white70)),
+                            const SizedBox(height: 20),
+                            Center(
+                                child: Image.asset('assets/images/payment_qr.png',
+                                    width: 200, height: 200)),
+                            const SizedBox(height: 20),
+                            buildTextField(
+                                "Transaction ID",
+                                "Enter Transaction ID",
+                                controller.transactionIdController),
+                            const SizedBox(height: 30),
+                            Obx(() => controller.isLoading.value
+                                ? const Center(
+                                    child: CircularProgressIndicator(
+                                        color: Colors.white))
+                                : GestureDetector(
+                                    onTap: () {
+                                      if (controller.transactionIdController.text
+                                          .isNotEmpty) {
+                                        controller.submitRegistration();
+                                      } else {
+                                        Get.snackbar("Missing Info",
+                                            "Please enter the Transaction ID.",
+                                            backgroundColor: Colors.red,
+                                            colorText: Colors.white);
+                                      }
+                                    },
+                                    child: actionButton("Submit",
+                                        Color(0xFF6C4AB6), Color(0xFFE96F6F)),
+                                  )),
+                          ],
+                        )),
+                  const SizedBox(height: 80),
+                ],
+              ),
             ),
           ),
         ),
